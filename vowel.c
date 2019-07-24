618f409523346ddebfaadb5728a4681618048892
jenetshirley <53214236+jenetshirley@users.noreply.github.com>
2019-07-24 15:30:27 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,13 +3,17 @@ int main()
 {
 char n;
 scanf("%c",&n);
-if(n="a","e","i","o","u")
+if(n=='a'||n=='e'||n=='i'||n=='o||n=='u')
 {
 printf("Vowel");
 }
-else
+else if(n!='a'||n!='e'||n!='i'||n!='o'||n!='u')
 {
 printf("Consonant");
 }
+else
+   {
+     printf("Invalid")
+   }
 return 0;
 }
