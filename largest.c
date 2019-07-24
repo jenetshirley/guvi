6bf3345802711d899a12b2b45217c1aa3bcdf80e
jenetshirley <53214236+jenetshirley@users.noreply.github.com>
2019-07-24 23:18:42 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int b,c,d;
+scanf("%d","%d","%d",&b,&c,&d);
+if((b>c)&&(b>d))
+{
+printf("%d",b);
+}
+else if(c>d)
+{
+printf("%d",c);
+}
+else
+{
+printf("%d",d);
+}
+}
