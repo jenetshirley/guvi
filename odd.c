15244e9a7fce79e316c7553da34177ec4ea152e7
jenetshirley <53214236+jenetshirley@users.noreply.github.com>
2019-07-24 15:10:17 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+    printf("Even");
+  }
+  else if(n%2!=0)
+  {
+    printf("Odd");
+  }
+  else
+  {
+    printf("invalid");
+  }
+  return 0;
+}
